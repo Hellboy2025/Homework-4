dd669dd65a3c0fba259c07b79f42c992291063ce
Hellboy2025 <bryan.almeida@westpoint.edu>
2024-09-10 21:07:53 -0400

    WIP-ProblemII

--- hw4/hw4.scala
+++ hw4/hw4.scala
@@ -1,3 +1,5 @@
+import javax.print.DocFlavor.INPUT_STREAM
+import scala.compiletime.ops.double
 object hw4 extends hwtest.hw("CS478"):
   def userName = "Bryan Almeida Betancourt"
 
@@ -47,6 +49,47 @@ object hw4 extends hwtest.hw("CS478"):
   def parse2(str: String): Option[Boolean] =
     // use a recursive helper function!
 
-    ???
+    def helper(index: Int): Option[(Boolean, Int)]=
+      if index >= str.length() then
+        return None
+      str(index) match
+        case'T'  => Some((true,index+1))
+        case'F' => Some((false,index+1))
+        case '|'=> 
+          val (left_value,nextIndex) <- helper(index+1)
+          val (right_value, finalIndex) <- helper(nextIndex)
+          return (left_value || right_value, finalIndex+1)
+        case '&'=>
+          for
+          (left_value,nextIndex) <- helper(index+1)
+          (right_value, finalIndex) <- helper(nextIndex)
+        yield(left_value && right_value, finalIndex+1)
+        case '!'=> 
+          for
+          (value,nextIndex)<- helper(index+1)
+          yield(!(value),nextIndex+1)
+          // Invalid character
+        case'('  =>
+          for
+          (value1, nextIndex) <- helper(index + 1) // Parse the left side
+          operator = str(nextIndex)                    // Get the operator
+          (value2, finalIndex) <- helper(nextIndex + 1) // Parse the right side
+          if str(finalIndex) == ')'              // Ensure closing parenthesis
+        yield (operator match {
+          case '&' => value1 && value2 // Logical AND
+          case '|' => value1 || value2 // Logical OR
+        }, finalIndex + 1)              // Move past the closing parenthesis
+        case _ => None
+        
 
-  ignoretest("parse2", parse2, "str")
+        
+              
+    
+    helper(0).flatMap {
+    case (result, finalIndex) =>
+      if (finalIndex == str.length) Some(result) // Return result if parsing complete
+      else None // Return None if extra characters are present
+    }
+
+
+  test("parse2", parse2, "str")
